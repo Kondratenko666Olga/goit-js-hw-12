f3e41d757f9cfb002d3b84aac5fa4a55b7038057
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-04-11 17:49:03 +0300

    axios_added

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-
-
 export async function fetchImages(query) {
     const API_KEY = '43258718-b5e692e88f6a0e01a4c3d4849';
     const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
