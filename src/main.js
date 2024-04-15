81b7988c0309b4dacc05c4dc0af527c4b5c4b8d4
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-04-15 23:19:00 +0300

    update

--- src/main.js
+++ src/main.js
@@ -124,7 +124,6 @@ function showLoadBtn() {
   }
 
   function checkBtnVisibleStatus() {
-    debugger;
     if (page >= maxPage) {
       hideLoadBtn();
       showError("We're sorry, but you've reached the end of search results.");
